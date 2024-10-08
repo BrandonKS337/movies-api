770ffdd5623fd9432b25551e26f306e536e26059
Project V2 <brandonks337@gmail.com>
2024-10-07 21:52:50 -0500

    first commit on the movies-api.

---
+++ src/main/java/dev/MovieProjectfun/movies/MoviesApplication.java
@@ -0,0 +1,23 @@
+package dev.MovieProjectfun.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+
+	//Using this as an example of setting up an endpoint example in Java.
+	// @GetMapping("/") //this is a way of saying that the apiRoot below is a GET endpoint. Defined by declaring the endpoint
+	// public String apiRoot() {
+	// 	return "Hello, World!"; 
+	// }
+
+}
